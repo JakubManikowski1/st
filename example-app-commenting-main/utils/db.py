7a683e2c27e655e313216b9ce0a0f38dd5d75ab5
JakubManikowski1 <65075858+JakubManikowski1@users.noreply.github.com>
2022-09-11 21:44:54 +0200

    Update db.py

--- example-app-commenting-main/utils/db.py
+++ example-app-commenting-main/utils/db.py
@@ -11,7 +11,7 @@ from googleapiclient.http import HttpRequest
 socket.setdefaulttimeout(15 * 60)
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1VzwuTdcVkEOrPs_by28pNAYoNilwco8eNMCW2bHbmSk"
+SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
 SHEET_NAME = "database"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
