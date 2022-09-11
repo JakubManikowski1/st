e55b9c28732d7ec8e5cc90f76a0359d8d529c3e9
JakubManikowski1 <65075858+JakubManikowski1@users.noreply.github.com>
2022-09-11 21:50:01 +0200

    OK

--- example-app-commenting-main/utils/db.py
+++ example-app-commenting-main/utils/db.py
@@ -12,7 +12,7 @@ socket.setdefaulttimeout(15 * 60)
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
 SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
-SHEET_NAME = "database"
+SHEET_NAME = "Database"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
 
